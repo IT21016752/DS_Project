4a46fa623ba194b832d28fcee40b3c80a97b991b
Arani12 <it21014536@my.sliit.lk>
2023-04-13 22:50:25 +0530

    2nd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddItem from './pages/ItemManagement/AddItem.js';
+import AddDeliveryInfo from './pages/DeliveryManagement/addDelInfo';
 import Header from './pages/Header.js';
-
 function App() {
   return (
     <Router>
@@ -11,10 +11,14 @@ function App() {
         <Route element={<Header />}>
           <Route path="/" />
           <Route path="/additems" element={<AddItem />} />
+          <Route path="/AddDelInfo" element={<AddDeliveryInfo />} />
         </Route>
       </Routes>
     </Router>
+   
+    
   );
+  
 }
 
 export default App;
