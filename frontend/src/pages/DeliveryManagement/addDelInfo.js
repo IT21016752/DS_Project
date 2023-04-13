e0792c9be78023c22252619ab8357d5e5367b56d
Arani12 <it21014536@my.sliit.lk>
2023-04-13 22:53:32 +0530

    added delivery

--- frontend/src/pages/DeliveryManagement/addDelInfo.js
+++ frontend/src/pages/DeliveryManagement/addDelInfo.js
@@ -4,24 +4,22 @@ export default function AddDeliveryInfo(){
 
     return(
 
-        <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="name"><i class="fa fa-user"></i> Contact Name</label>
-            <input type="text" id="name" name="name" placeholder="John M. Doe"/>
-            <label>   
-            Phone :  
-            </label>  
-            <input type="text" name="country code"  value="+94" size="2"/>   
-            <input type="text" name="phone" size="10"/>  <br/>  
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"/>
-            <label for="province"><i class="fa fa-institution"></i> Province</label>
-            <input type="text" id="province" name="province" placeholder="Central"/>
-            <label for="Amount"> Amount</label>
-            <input type="text" id="amount" name="amount" placeholder="Rs. 1000/="/>
-            <label for="time"> Time</label>
-            <input type="text" id="amount" name="amount" placeholder="A.M. or P.M."/>
-          </div>
+        <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="Text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label></input>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     )
 
 
