cdb609db3bc187bab1d839fbd325107e965d0fda
IT21016752 <it21016752@my.sliit.lk>
2023-04-15 10:51:48 +0530

    Item Management - Read Data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddItem from './pages/ItemManagement/AddItem.js';
 import Header from './pages/Header.js';
+import AllItems from './pages/ItemManagement/AllItems.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Header />}>
           <Route path="/" />
           <Route path="/additems" element={<AddItem />} />
+          <Route path="/allitems" element={<AllItems />} />
         </Route>
       </Routes>
     </Router>
