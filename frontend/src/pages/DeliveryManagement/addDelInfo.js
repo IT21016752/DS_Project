de050bfbd69f85ef3587a1c7cb7d6f897150222f
Arani12 <it21014536@my.sliit.lk>
2023-04-15 23:52:38 +0530

    added after error resolved 2

--- frontend/src/pages/DeliveryManagement/addDelInfo.js
+++ frontend/src/pages/DeliveryManagement/addDelInfo.js
@@ -3,23 +3,37 @@ import React,{useState} from "react"
 export default function AddDeliveryInfo(){
 
     return(
-
+<div className="container">
         <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="exampleInputname">Contact name:</label>
+    <input type="text" class="form-control" id="exampleInputname"  placeholder="Enter Contact name"/>
+   
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="Text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  <label for="exampleInputAddress">Address:</label>
+    <input type="text" class="form-control" id="exampleInputAddress"  placeholder="34/123 nobel place ,kamdy"/>
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label></input>
+  <div class="form-group">
+  <label for="exampleInputProvince">Province:</label>
+    <input type="text" class="form-control" id="exampleInputProvince"  placeholder="Ex:Central"/>
   </div>
+  <div class="form-group">
+  <label for="exampleInputPhone">Phone:</label>
+    <input type="text" class="form-control" id="exampleInputPhone"  placeholder="+94 754236541"/>
+  </div>
+  <div class="form-group">
+  <label for="exampleInputPrice">Price:</label>
+    <input type="text" class="form-control" id="exampleInputPrice"  placeholder="Rs:2000 /-"/>
+  </div>
+  <div class="form-group">
+  <label for="exampleInputTime">Time:</label>
+    <input type="text" class="form-control" id="exampleInputTime"  placeholder="tt"/>
+  </div>
+  
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</div>
     )
 
 
