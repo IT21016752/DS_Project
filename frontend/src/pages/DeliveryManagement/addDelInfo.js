2b7827fc119e00d331a15dc117cd9a20fe9f58f7
Arani12 <it21014536@my.sliit.lk>
2023-04-16 18:17:29 +0530

    commit 2

--- frontend/src/pages/DeliveryManagement/addDelInfo.js
+++ frontend/src/pages/DeliveryManagement/addDelInfo.js
@@ -1,34 +1,81 @@
 import React,{useState} from "react"
+import axios from "axios";
 
 export default function AddDeliveryInfo(){
 
+
+  const [Cname,setName]= useState("");
+  const [Address,setAddress]= useState("");
+  const [Province,setProvince]= useState("");
+  const [Phone,setPhone]= useState("");
+  const [Price,setPrice]= useState("");
+  const [Time,setTime]= useState("");
+
+function sendData(e){
+  e.preventDefault(); 
+
+  const newDelivery ={
+    Cname,
+    Address,
+    Province,
+    Phone,
+    Price,
+    Time,
+
+  }
+
+  axios.post("http://localhost:8091/delivery/add",newDelivery).then(()=>{
+    alert("delivery infromation added")
+  }).catch((err)=>{
+    alert(err)
+  })
+}
+
     return(
 <div className="container">
-        <form>
+  <form onSubmit={sendData}>
   <div class="form-group">
-    <label for="exampleInputname">Contact name:</label>
-    <input type="text" class="form-control" id="exampleInputname"  placeholder="Enter Contact name"/>
+    <label for="Cname">Contact name:</label>
+    <input type="text" class="form-control" id="Cname"  placeholder="Enter Contact name" onChange={(e)=>{
+
+setName(e.target.value);
+    }}/>
    
   </div>
   <div class="form-group">
-  <label for="exampleInputAddress">Address:</label>
-    <input type="text" class="form-control" id="exampleInputAddress"  placeholder="34/123 nobel place ,kamdy"/>
+  <label for="Address">Address:</label>
+    <input type="text" class="form-control" id="Address"  placeholder="34/123 nobel place ,kamdy"  onChange={(e)=>{
+
+setAddress(e.target.value);
+    }}/>
   </div>
   <div class="form-group">
-  <label for="exampleInputProvince">Province:</label>
-    <input type="text" class="form-control" id="exampleInputProvince"  placeholder="Ex:Central"/>
+  <label for="Province">Province:</label>
+    <input type="text" class="form-control" id="Province"  placeholder="Ex:Central"onChange={(e)=>{
+
+setProvince(e.target.value);
+    }}/>
   </div>
   <div class="form-group">
-  <label for="exampleInputPhone">Phone:</label>
-    <input type="text" class="form-control" id="exampleInputPhone"  placeholder="+94 754236541"/>
+  <label for="Phone">Phone:</label>
+    <input type="text" class="form-control" id="Phone"  placeholder="+94 754236541"  onChange={(e)=>{
+
+setPhone(e.target.value);
+    }}/>
   </div>
   <div class="form-group">
-  <label for="exampleInputPrice">Price:</label>
-    <input type="text" class="form-control" id="exampleInputPrice"  placeholder="Rs:2000 /-"/>
+  <label for="Price">Price:</label>
+    <input type="text" class="form-control" id="Price"  placeholder="Rs:2000 /-"  onChange={(e)=>{
+
+setPrice(e.target.value);
+    }}/>
   </div>
   <div class="form-group">
-  <label for="exampleInputTime">Time:</label>
-    <input type="text" class="form-control" id="exampleInputTime"  placeholder="tt"/>
+  <label for="Time">Time:</label>
+    <input type="text" class="form-control" id="Time"  placeholder="tt"  onChange={(e)=>{
+
+setTime(e.target.value);
+    }}/>
   </div>
   
   <button type="submit" class="btn btn-primary">Submit</button>
